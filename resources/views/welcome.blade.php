33c9a2c98631d54df611bf4f42453f8b5f804a92
dimitris <dd@ds-Macbook-Pro.local>
2017-12-07 01:05:44 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title','Acme Bookstore')
+
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Welcome
+            </div>
+        </div>
+    </div>
+@endsection
+
